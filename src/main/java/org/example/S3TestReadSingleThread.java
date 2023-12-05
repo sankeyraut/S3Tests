783258827689d87cc6ac5fe20215bda54ac4c32c
Sanket Raut <sankerau@amazon.com>
2023-12-05 13:14:18 +0530

    add: Simple Execution

---
+++ src/main/java/org/example/S3TestReadSingleThread.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class S3TestReadSingleThread {
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+        int roundId = 12345;
+
+        ObjectMapper mapper = new ObjectMapper();
+        URI myURI = new URI("https://s3express-usw2-az1.us-west-2.amazonaws.com");
+
+        S3Client client = S3Client.builder()
+                .region(Region.US_WEST_2)
+                .endpointOverride(myURI)
+                .build();
+
+
+        for(int i=0;i<50;i++){
+            File teamsFile = new File(i + "-teams.json");
+            GetObjectRequest objectRequest = GetObjectRequest.builder()
+                    .bucket(Constants.BUCKET_NAME)
+                    .key(i+"")
+                    .build();
+            long start = System.nanoTime();
+            client.getObject(objectRequest,teamsFile.toPath());
+            long end = System.nanoTime();
+            System.out.println("Operation completed  "+ (end-start)/1000000+" milliseconds");
+
+
+        }
+
+
+
+    }
+
+
+
+}
